e3fb12e4556911cb48cd04f1126f50990dd6626c
auqez <89996159+techlifeadvice@users.noreply.github.com>
2021-09-15 18:47:38 +0200

    added sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
-
+    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
     eleventyConfig.addFilter("postDate", (dateObj)=>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED); 
 });
