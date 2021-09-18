65fb277f6227d3dd5f2c679a88f70e15a6eb9ad5
auqez <89996159+techlifeadvice@users.noreply.github.com>
2021-09-18 15:06:35 +0200

    hm

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
+    eleventyConfig.addPassthroughCopy('./src/db.js');
     eleventyConfig.addFilter("postDate", (dateObj)=>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED); 
 });
